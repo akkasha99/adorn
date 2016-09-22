1469e1bb97b36c81c420b313856a2f78892d668b
asad <asad.rehman@ilsainteractive.is>
2016-09-22 19:01:52 +0500

    commit

--- db/migrate/20141120131506_add_is_blocked_to_user_feeds.rb
+++ db/migrate/20141120131506_add_is_blocked_to_user_feeds.rb
@@ -1,5 +1,5 @@
 class AddIsBlockedToUserFeeds < ActiveRecord::Migration
   def change
-    add_column :user_feeds, :is_active, :boolean , :default => true
+    add_column :user_feeds, :is_active, :integer , :default => 2
   end
 end
