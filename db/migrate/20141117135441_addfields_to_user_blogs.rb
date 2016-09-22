c05379d3720fa8d7a0868f209c0a22d503fb6524
asad <asad.rehman@ilsainteractive.is>
2016-09-22 17:44:57 +0500

    musibat

--- db/migrate/20141117135441_addfields_to_user_blogs.rb
+++ db/migrate/20141117135441_addfields_to_user_blogs.rb
@@ -1,7 +1,7 @@
 class AddfieldsToUserBlogs < ActiveRecord::Migration
   def change
     add_column :user_blogs, :title, :string
-    add_column :user_blogs, :content, :text, :limit => 4294967295
+    add_column :user_blogs, :content, :text, :limit => 1073741823
     add_column :user_blogs, :identifier, :string
     add_column :user_blogs, :web_link, :string
   end
