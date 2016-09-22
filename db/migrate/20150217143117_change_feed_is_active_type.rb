64a9bdd8ebdde5658d0ae3aacd651bb2f0a2287c
asad <asad.rehman@ilsainteractive.is>
2016-09-22 18:52:34 +0500

    same

--- db/migrate/20150217143117_change_feed_is_active_type.rb
+++ db/migrate/20150217143117_change_feed_is_active_type.rb
@@ -1,8 +1,8 @@
 class ChangeFeedIsActiveType < ActiveRecord::Migration
   def up
     # change_column :user_feeds, :is_active, :integer, :default => 2    ##for mysql
-    change_column :user_feeds, :is_active, 'integer USING CAST(is_active AS integer)', :default => 2
-    # change_column :user_feeds, :is_active, :default => 2
+    change_column :user_feeds, :is_active,'integer USING CAST(is_active AS integer)'
+    change_column :user_feeds, :is_active, :default => 2
   end
 
   def down
