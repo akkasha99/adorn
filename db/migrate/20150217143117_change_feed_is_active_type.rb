ccc828de0701fc2016044914dff1f48662c7aaa8
asad <asad.rehman@ilsainteractive.is>
2016-09-22 18:32:35 +0500

    same as last commit

--- db/migrate/20150217143117_change_feed_is_active_type.rb
+++ db/migrate/20150217143117_change_feed_is_active_type.rb
@@ -1,6 +1,7 @@
 class ChangeFeedIsActiveType < ActiveRecord::Migration
   def up
-    change_column :user_feeds, :is_active, :integer, :default => 2
+    # change_column :user_feeds, :is_active, :integer, :default => 2    ##for mysql
+    change_column :user_feeds, :is_active, USING CAST(is_active AS integer), :default => 2
   end
 
   def down
