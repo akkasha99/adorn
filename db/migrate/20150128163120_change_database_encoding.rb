bcdc4cd0ffd447e180b6acd2d6ac732555a55b98
asad <asad.rehman@ilsainteractive.is>
2016-09-22 18:25:34 +0500

    utf encoding commented

--- db/migrate/20150128163120_change_database_encoding.rb
+++ db/migrate/20150128163120_change_database_encoding.rb
@@ -8,15 +8,15 @@ class ChangeDatabaseEncoding < ActiveRecord::Migration
   }
 
   def self.up
-    execute "ALTER DATABASE '#{ActiveRecord::Base.connection.current_database}' CHARACTER SET utf8mb4;"
-
-    ActiveRecord::Base.connection.tables.each do |table|
-      execute "ALTER TABLE '#{table}' CHARACTER SET = utf8mb4;"
-    end
-
-    UTF8_PAIRS.each do |table, col|
-      execute "ALTER TABLE '#{table}' CHANGE '#{col}' '#{col}' TEXT  CHARACTER SET utf8mb4  NULL;"
-    end
+    # execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4;"
+    #
+    # ActiveRecord::Base.connection.tables.each do |table|
+    #   execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
+    # end
+    #
+    # UTF8_PAIRS.each do |table, col|
+    #   execute "ALTER TABLE `#{table}` CHANGE `#{col}` `#{col}` TEXT  CHARACTER SET utf8mb4  NULL;"
+    # end
 
   end
 
